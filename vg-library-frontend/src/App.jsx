78b8d245520f37c82f5566456480856d5de95b94
Alejandro Maldonado <am@AM-WEBDEV.attlocal.net>
2024-05-01 17:27:50 -0500

    experimenting with tailwind styles

--- vg-library-frontend/src/App.jsx
+++ vg-library-frontend/src/App.jsx
@@ -4,7 +4,7 @@ import VideoGameList from './components/VideoGameList';
 function App() {
 
   return (
-    <div className='bg-red-300'>
+    <div className='bg-gray-300'>
       <h1 className="font-bold p-8 text-center text-3xl">Video Game Library</h1>
       <VideoGameList />
     </div>
--- vg-library-frontend/src/components/VideoGameList.jsx
+++ vg-library-frontend/src/components/VideoGameList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 
+//React functional component 1)utilizing useState hook to create a state variable and a setter function, and 2)useEffect hook to fetch data after the component mounts and updates state
 const VideoGameList = () => {
   const [videoGames, setVideoGames] = useState([]);
 
@@ -10,12 +11,12 @@ const VideoGameList = () => {
   }, []);
 
   return (
-    <div className="bg-gray-200 p-8">
-      <h1 className="text-2xl font-bold">Video Games</h1>
+    <div className="bg-black p-8">
+      <h1 className="text-2xl font-bold text-white">Video Games (1990-2009)</h1>
       <ul className="mt-4">
         {videoGames.map(videoGame => (
-          <li key={videoGame.id} className="text-lg text-gray-800">
-            {videoGame.title} - {videoGame.developer} ({videoGame.year})
+          <li key={videoGame.id} className="text-lg text-white">
+            {videoGame.rank}: {videoGame.title} - {videoGame.developer} ({videoGame.year})
           </li>
         ))}
       </ul>
